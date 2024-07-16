0d873cd0dbe8c3fef32e7f373a0aa8da364d9688
hassanqamar27 <hassanqamer2002@gmail.com>
2024-07-17 04:46:07 +0500

    Dom Assignment Commit

---
+++ 01-ColorChanger/app.js
@@ -0,0 +1,19 @@
+const button = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+button.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    if (e.target.id === "grey") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "white") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "blue") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
---
+++ 06-UnlimitedColors/app.js
@@ -0,0 +1,23 @@
+const randomColor = () => {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+let intervalId;
+const startChangingColor = () => {
+  if (!intervalId) {
+    intervalId = setInterval(() => {
+      document.body.style.backgroundColor = randomColor();
+      //   console.log(randomColor());
+    }, 1000);
+  }
+};
+const stopChangingColor = () => {
+  clearInterval(intervalId);
+  intervalId = null;
+};
+document.querySelector("#start").addEventListener("click", startChangingColor);
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
